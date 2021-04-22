bb0ea03dce721f1de997698a8d245ea9e9e98e88
suryaadi44 <suryawarior44@gmail.com>
2021-04-22 11:50:56 +0800

    Fixing auto directory creation destination

--- lib/directory.c
+++ lib/directory.c
@@ -3,7 +3,15 @@
 void directoryCheck() {
     struct stat st = { 0 };
 
-    if (stat("../db_rek", &st) == -1) {
-        mkdir("../db_rek");
+    if (stat("db_rek", &st) == -1) {
+        mkdir("db_rek");
+    }
+
+    if (stat("db_user", &st) == -1) {
+        mkdir("db_user");
+    }
+
+    if (stat("rek_record", &st) == -1) {
+        mkdir("rek_record");
     }
 }
